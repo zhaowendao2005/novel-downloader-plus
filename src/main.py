5c68527c5a69ae1d8a0a882ebdb094c6fc34960d
zhaowendao2005 <140528990+zhaowendao2005@users.noreply.github.com>
2025-02-21 18:11:20 +0800

    修改下载按钮

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from PyQt5 import QtCore, QtWidgets
-from QT.main_window.windows_glass import Ui_MainWindow
+from qfluentwidgets import FluentIcon
+from qfluentwidgets.common import Icon
 
+
+from QT.main_window.windows1 import Ui_MainWindow
+from qfluentwidgets.components import PushButton
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # Setup the UI from the imported class
-
+        self.pushButton_downloader.setIcon(Icon(FluentIcon.DOWN))
 
 if __name__ == "__main__":
     import sys
